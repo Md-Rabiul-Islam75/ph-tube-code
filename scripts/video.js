f04f30a9219b1ed4e48a40a5c0ec182f33644a6d
Md. Rabiul Islam <rabiul77777888@gmail.com>
2024-10-09 18:25:41 +0600

    Load Categories And Send Data To Display

---
+++ scripts/video.js
@@ -0,0 +1,18 @@
+// 1 - Fetch, Load and Show Categories on html
+
+// create loadCategories
+const loadCategories = () => {
+    //fetch the data
+    fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then(res => res.json())
+    .then(data => displayCategories(data.categories))
+    .catch(error => console.log(error));
+};
+
+// Create displayCategories
+const displayCategories = (data) => {
+    //add Data in html
+    console.log(data);
+};
+
+loadCategories();
