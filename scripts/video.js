5539d51c22593eb4f0abff491eb5500a3601fdc7
Md. Rabiul Islam <rabiul77777888@gmail.com>
2024-10-09 23:18:16 +0600

    Recap The Mechanism , Load Videos And Display Cards On The Ui

--- scripts/video.js
+++ scripts/video.js
@@ -9,6 +9,44 @@ const loadCategories = () => {
     .catch(error => console.log(error));
 };
 
+const loadVideos = () => {
+    //fetch the data
+    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then(res => res.json())
+    .then(data => displayVideos(data.videos))
+    .catch((error) => console.log(error));
+
+};
+
+const displayVideos = (videos) => {
+    const videoContainer = document.getElementById("videos");
+    videos.forEach((video) => {
+        console.log(video);
+        const card = document.createElement("div");
+        card.classList = "card card-compact"
+        
+        card.innerHTML = `
+        <figure>
+    <img
+      src=${video.thumbnail}
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+        
+        `;
+
+        videoContainer.append(card);
+    });
+
+    //console.log(videos);
+};
+
 // Create displayCategories
 const displayCategories = (categories) => {
     const categoryContainer = document.getElementById("categories");
@@ -28,3 +66,4 @@ const displayCategories = (categories) => {
 };
 
 loadCategories();
+loadVideos();
